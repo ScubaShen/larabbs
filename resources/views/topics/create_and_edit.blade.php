af67222fc9432e0ad9c35b52bfe4ec9115dd6470
Scuba <cheeserainbow@163.com>
2018-05-24 10:10:47 +0000

    User can upload topic image

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -70,6 +70,14 @@
     $(document).ready(function(){
       var editor = new Simditor({
         textarea: $('#editor'),
+        upload: {
+          url: '{{ route('topics.upload_image') }}',
+          params: { _token: '{{ csrf_token() }}' },
+          fileKey: 'upload_file',  //服务器端获取图片的键值
+          connectionCount: 3,  //最多只能同时上传 3 张
+          leaveConfirm: '文件上传中，关闭此页面将取消上传。'
+        },
+        pasteImage: true,  //支持图片複製貼上上传
       });
     });
   </script>
