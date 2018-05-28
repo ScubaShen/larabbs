e6b789d0a3f8ef6a34c6b4c0bde645fe5c2f88eb
Scuba <cheeserainbow@163.com>
2018-05-28 06:17:11 +0000

    Notify author when topic replied

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -14,9 +15,13 @@ class ReplyObserver
         $reply->content = clean($reply->content, 'user_topic_body');  //配合config/purifier 使用
     }
 
-    public function created(Reply $reply)
+    public function created(Reply $reply)  //在創建回復之後
     {
-        $reply->topic->increment('reply_count', 1);
+        $topic = $reply->topic;
+        $topic->increment('reply_count', 1);
+
+        // 通知作者话题被回复了
+        $topic->user->notify(new TopicReplied($reply));
     }
 
     public function updating(Reply $reply)
